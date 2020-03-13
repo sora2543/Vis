938d05da106eb225258ea514cfa0a80a73474ee8
sora aoi <k-o-a-t-i@hotmail.com>
2020-03-13 21:43:49 +0700

    add code

---
+++ Work4/Data.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Work4
+{
+    public partial class Data : Form
+    {
+        public Data()
+        {
+            InitializeComponent();
+        }
+
+        private void Data_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql = " SELECT * FROM data";
+            sql = "INSERT INTO data (user,list,address,phone) VALUES('" + textBox1.Text + "','" + textBox4.Text + "','" + textBox2.Text + "','" + textBox3.Text + "')";
+            MySqlConnection con = new MySqlConnection("host=localhost;user=min;password=0854559377as;database=transportgoodies");
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            con.Open();           
+                //MessageBox.Show("Login ได้แล้ว");
+                this.Hide();
+                Main f = new Main();
+                f.Show();            
+
+            /*MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                MessageBox.Show(reader.GetString("user") + " " + reader.GetString("list") +" "+ reader.GetString("address") + " " + reader.GetString("phone"));
+            }*/
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Main f = new Main();
+            f.Show();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Work4/Main.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Work4
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(60, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(248, 83);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ส่งอาหาร-เครื่องดื่ม";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(60, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(248, 82);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ส่งเอกสาร-พัสดุ\r\n";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(60, 289);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(248, 82);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "เว็บไซต์";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 442);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Main";
+            this.Text = "K....";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+    }
+}
